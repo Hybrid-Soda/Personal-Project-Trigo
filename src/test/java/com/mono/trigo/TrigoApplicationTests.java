4c93c9dddfa57f8b05cf75ddc9e0df555d7eb754
임경태 <limkt0925@gmail.com>
2024-12-31 00:53:31 +0900

    Feat: Spring Boot Init

---
+++ src/test/java/com/mono/trigo/TrigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mono.trigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
