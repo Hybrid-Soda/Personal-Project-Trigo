2410a7aaa72f363a1f63e63d4541928327316edf
임경태 <limkt0925@gmail.com>
2025-01-01 00:57:28 +0900

    Feat: UserEntity 구현

--- src/main/java/com/mono/trigo/domain/user/entity/User.java
+++ src/main/java/com/mono/trigo/domain/user/entity/User.java
@@ -1,4 +1,40 @@
 package com.mono.trigo.domain.user.entity;
 
-public class User {
+import com.mono.trigo.common.audit.BaseEntity;
+
+import lombok.*;
+import jakarta.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "Users")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String nickname;
+
+    @Column(nullable = false, length = 10)
+    private String birthday;
+
+    @Column(nullable = false)
+    private Boolean gender;
+
+    @Column
+    private String location;
+
+    @Column(nullable = false, length = 50)
+    private String role = "member";
+
 }
