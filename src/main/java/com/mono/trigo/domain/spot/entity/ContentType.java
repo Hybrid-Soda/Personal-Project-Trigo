79d1c311f2f7d9136c50a05865e24156391a9972
임경태 <limkt0925@gmail.com>
2025-01-01 20:17:28 +0900

    Feat: ContentTypeEntity 구현

---
+++ src/main/java/com/mono/trigo/domain/spot/entity/ContentType.java
@@ -0,0 +1,30 @@
+package com.mono.trigo.domain.spot.entity;
+
+import lombok.*;
+import jakarta.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "ContentTypes")
+public class ContentType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "content_type_id")
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String cat1;
+
+    @Column(nullable = false, length = 20)
+    private String cat2;
+
+    @Column(nullable = false, length = 20)
+    private String cat3;
+
+    @Column(nullable = false)
+    private String name;
+
+}
