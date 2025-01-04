ddbf7862071aadb7d78a8fb39a8d65519551011b
임경태 <limkt0925@gmail.com>
2025-01-05 02:24:39 +0900

    Feat: SecurityConfig 수정

--- src/main/java/com/mono/trigo/web/config/SecurityConfig.java
+++ src/main/java/com/mono/trigo/web/config/SecurityConfig.java
@@ -2,34 +2,43 @@ package com.mono.trigo.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private CorsConfig corsConfig;
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .addFilter(corsConfig.corsFilter())
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/api/**").permitAll()
+                // csrf disable
+                .csrf(AbstractHttpConfigurer::disable)
+                // form login disable
+                .formLogin(AbstractHttpConfigurer::disable)
+                // http basic auth disable
+                .httpBasic(AbstractHttpConfigurer::disable)
+                // 경로별 인가 작업
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/users/signup").permitAll()
+                        .requestMatchers("/login", "/", "/signup").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .oauth2ResourceServer((oauth2) -> oauth2
-                        .jwt(Customizer.withDefaults()))
+                // 세션 설정
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
 
