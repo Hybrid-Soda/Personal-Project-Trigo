bca3877e28e1139aec82063169c66f3d20139a4f
임경태 <limkt0925@gmail.com>
2025-01-05 02:11:48 +0900

    Feat: signup 구현

--- src/main/java/com/mono/trigo/web/user/controller/UserController.java
+++ src/main/java/com/mono/trigo/web/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.mono.trigo.web.user.controller;
 
 import com.mono.trigo.web.user.dto.SignupRequest;
+import com.mono.trigo.web.user.dto.SignupResponse;
 import com.mono.trigo.web.user.service.UserService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -18,8 +19,9 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody SignupRequest signupRequest) {
-        return ResponseEntity.status(201).body("ok");
+    public ResponseEntity<SignupResponse> signup(@RequestBody SignupRequest signupRequest) {
+        SignupResponse response = userService.signup(signupRequest);
+        return ResponseEntity.status(201).body(response);
     }
 
 }
--- src/main/java/com/mono/trigo/web/user/service/UserService.java
+++ src/main/java/com/mono/trigo/web/user/service/UserService.java
@@ -1,8 +1,10 @@
 package com.mono.trigo.web.user.service;
 
 import com.mono.trigo.domain.user.entity.User;
+import com.mono.trigo.domain.user.repository.UserRepository;
 import com.mono.trigo.web.user.dto.SignupRequest;
 
+import com.mono.trigo.web.user.dto.SignupResponse;
 import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
 
@@ -13,11 +15,24 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
 
-    public void signup(SignupRequest signupRequest) {
+    private final UserRepository userRepository;
+
+    public SignupResponse signup(SignupRequest signupRequest) {
 
         User user = User.builder()
-                .
+                .email(signupRequest.getEmail())
+                .password(signupRequest.getPassword())
+                .nickname(signupRequest.getNickname())
+                .birthday(signupRequest.getBirthday())
+                .gender(signupRequest.getGender())
+                .location(signupRequest.getLocation())
+                .build();
 
+        User savedUser = userRepository.save(user);
+
+        return SignupResponse.builder()
+                .userId(savedUser.getId())
+                .build();
     }
 
 }
